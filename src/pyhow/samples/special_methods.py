4b74198d3c7277fd5a88194768febd6f0d84919e
yoeo <yandaar@laposte.net>
2016-03-18 00:45:12 +0100

    Add copy and serialization to special methods

--- src/pyhow/samples/special_methods.py
+++ src/pyhow/samples/special_methods.py
@@ -3,7 +3,9 @@
 # using exemple classes that do not have public methods
 # pylint: disable=too-few-public-methods
 
+import copy
 import math
+import pickle
 import weakref
 
 
@@ -537,7 +539,7 @@ def qualname():
 
 
 def func():
-    """ obj.func.__func__: The unbound version of an instance method. """
+    """ obj.meth.__func__: The unbound version of an instance method. """
 
     class _ItemClass:
         def _function(self):
@@ -548,7 +550,7 @@ def func():
 
 
 def self_attribute():
-    """ obj.func.__self__: Instance bounded to a method. """
+    """ obj.meth.__self__: Instance bounded to a method. """
 
     class _ItemClass:
         def _function(self):
@@ -704,35 +706,127 @@ def trunc_method():
     return math.trunc(_MinusPi())
 
 
-# category: serialization
+# category: copy and serialization
 
 
-def copy():
-    """ __copy__: Todo. """
+def copy_method():
+    """ copy.copy(obj): Copy the content of an item. """
+
+    class _Copyable:
+        def __init__(self, value):
+            self.value = value
+        def __copy__(self):
+            return _Copyable(self.value)
+
+    return copy.copy(_Copyable("movie soundtrack")).value
 
 
-def deepcopy():
-    """ __deepcopy__: Todo. """
+def deepcopy_method():
+    """ copy.deepcopy(obj): Recursively copy the content of an item. """
+
+    class _Copyable:
+        def __init__(self, values):
+            self.values = values
+        def __deepcopy__(self, memory):
+            return _Copyable(copy.deepcopy(self.values, memo=memory))
+
+    return '~'.join(copy.deepcopy(_Copyable(['anime', 'opening'])).values)
 
 
-def getstate():
-    """ __getstate__: Todo. """
+def getstate_method():
+    """ pickle.dumps(obj): Get the "__dict__" of the serialized instance. """
+
+    class _Serializable:
+        def __getstate__(self):
+            return {'value': "series credits song"}
+
+    globals()['_Serializable'] = _Serializable
+    value = pickle.loads(pickle.dumps(_Serializable())).value
+    del globals()['_Serializable']
+
+    return value
 
 
-def reduce():
-    """ __reduce__: Todo. """
+def reduce_method():
+    """ pickle.dumps(obj): Info needed to rebuild the serialized instance. """
+
+    def _build(value):
+        item = _Serializable()
+        item.value = "sorry for the {}".format(value)
+        return item
+
+    class _Serializable:
+        def __reduce__(self):
+            return (_build, ('intermission',))
+
+    globals()['_build'] = _build
+    globals()['_Serializable'] = _Serializable
+    value = pickle.loads(pickle.dumps(_Serializable())).value
+    del globals()['_build']
+    del globals()['_Serializable']
+
+    return value
 
 
-def reduce_ex():
-    """ __reduce_ex__: Todo. """
+def reduce_ex_method():
+    """ pickle.dumps(obj): Same as "__reduce__" with protocole version. """
+
+    def _build(value):
+        item = _Serializable()
+        item.value = "sorry for the {}".format(value)
+        return item
+
+    class _Serializable:
+        def __reduce_ex__(self, protocol_version):
+            if protocol_version == 0:
+                return (_build, ('long intro music',))
+            else:
+                return (_Serializable.__new__, (_Serializable,))
+
+    globals()['_build'] = _build
+    globals()['_Serializable'] = _Serializable
+    value = pickle.loads(pickle.dumps(_Serializable(), 0)).value
+    del globals()['_build']
+    del globals()['_Serializable']
+
+    return value
 
 
-def getnewargs():
-    """ __getnewargs__: Todo. """
+def getnewargs_method():
+    """ pickle.loads(dump): "__new__" method attributes when unserializing. """
+
+    class _Serializable:
+        def __new__(cls, value=None):
+            item = object.__new__(cls)
+            if value:
+                item.value = value
+            return item
+        def __getnewargs__(self):
+            return ("Bollywood filmi music",)
+
+    globals()['_Serializable'] = _Serializable
+    value = pickle.loads(pickle.dumps(_Serializable())).value
+    del globals()['_Serializable']
+
+    return value
 
 
-def setstate():
-    """ __setstate__: Todo. """
+def setstate_method():
+    """ pickle.loads(dump): Set the "__dict__" of the deserialized instance. """
+
+    # __setstate__ is called only when bool(obj.__getstate__()) is True
+    class _Serializable:
+        def __getstate__(self):
+            return {'value': ''}
+        def __setstate__(self, instance_dict):
+            instance_dict.update({'value': "the ending of the short film"})
+            self.__dict__.update(instance_dict)
+
+    globals()['_Serializable'] = _Serializable
+    value = pickle.loads(pickle.dumps(_Serializable())).value
+    del globals()['_Serializable']
+
+    return value
 
 
 # category: resource management
